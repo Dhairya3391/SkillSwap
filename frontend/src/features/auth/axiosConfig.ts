1b9b03b9fe4117f88d8463d2f3471c14f552e00f
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2025-07-12 17:35:11 +0530

    Deployed Backend

--- frontend/src/features/auth/axiosConfig.ts
+++ frontend/src/features/auth/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://skillswap-1dkw.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
